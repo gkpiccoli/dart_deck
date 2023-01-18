a7fb758ffcb2c419c362d49c2c381e0621e24d5c
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-18 15:21:33 -0300

    add for loops

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,14 @@ void main() {
   var deck = new Deck();
 
   Deck() {
-    
+    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
+    var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
+
+    for (var suit in suits) {
+      for (var rank in ranks){
+        
+      }
+    }
   }
 }
 
