2816e84e9c827785cbbf7f30a4a02e281fb85339
Gustavo K. Piccoli <gustavokpiccoli@gmail.com>
2023-01-18 15:37:22 -0300

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,24 @@
-void main() {
-  var deck = new Deck();
+void main() {}
+var deck = new Deck();
 
+class Deck {
+  late List<Card> cards;
   Deck() {
     var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five'];
     var suits = ['Diamonds', 'Hearts', 'Clubs', 'Spades'];
 
     for (var suit in suits) {
-      for (var rank in ranks){
-        
+      for (var rank in ranks) {
+        var card = new Card(rank, suit);
+        cards.add(card);
       }
     }
   }
 }
 
-class Deck {
-  late List<Card> cards;
-  String? name;
-}
-
 class Card {
   String? suit;
   String? rank;
+
+  Card(this.rank, this.suit);
 }
